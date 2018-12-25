07962ea61defa3fdaac87d21fcc1711864b39051
horsun <im7h1995@hotmail.com>
2018-12-25 10:19:49 +0800

    监督学习---分类

--- Machine learning/监督学习/分类.py
+++ Machine learning/监督学习/分类.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import make_blobs
+
 # url:https://wizardforcel.gitbooks.io/scipycon-2018-sklearn-tut/content/5.html
 x, y = make_blobs(centers=2, random_state=0, cluster_std=1.5)
 print("x:", x)
@@ -9,14 +10,14 @@ print('first 5 samples: \n', x[:5, :])
 print(x[:5])
 import matplotlib.pyplot as plt
 
-# plt.figure(figsize=(8, 8))
+plt.figure(figsize=(8, 8))
 # [y==0,0] 第一个0 代表 y的value 是否为零 第二个 某列
-# plt.scatter(x[y == 0, 0], x[y == 0, 1], s=40, label='0')
-# plt.scatter(x[y == 1, 0], x[y == 1, 1], s=40, label='1', marker='s')
-# plt.xlabel('first feature')
-# plt.ylabel('second feature')
-# plt.legend(loc='upper right')
-# plt.show()
+plt.scatter(x[y == 0, 0], x[y == 0, 1], s=40, label='0')
+plt.scatter(x[y == 1, 0], x[y == 1, 1], s=40, label='1', marker='s')
+plt.xlabel('first feature')
+plt.ylabel('second feature')
+plt.legend(loc='upper right')
+plt.show()
 
 # 训练模型
 from sklearn.model_selection import train_test_split
---
+++ Machine learning/监督学习/回归分析.py
@@ -0,0 +1,26 @@
+# url :https://wizardforcel.gitbooks.io/scipycon-2018-sklearn-tut/content/6.html
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 从-3 -> 3 中取出 100条随机数
+x = np.linspace(-3, 3, 100)
+
+print(x)
+
+rng = np.random.RandomState(0)
+print(rng.uniform(size=len(x)))
+y1 = np.sin(4 * x)
+y2 = np.sin(4 * x) + x
+y = np.sin(4 * x) + x + rng.uniform(size=len(x))
+
+plt.plot(x, y1, 'o')
+plt.title('normal sin()')
+plt.show()
+
+plt.plot(x, y2, 'o')
+plt.title('sin() + x ')
+plt.show()
+
+plt.plot(x, y, 'o')
+plt.title('sin()+ x + random num')
+plt.show()
